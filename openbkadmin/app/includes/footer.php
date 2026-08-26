52c067c7fd6f6a15ab51a113b5eb778a8e9f2156
cyberrep <forum@rodrigotadeu.com.br>
2026-08-26 16:36:30 -0300

    Show add-on version in footer

--- openbkadmin/app/includes/footer.php
+++ openbkadmin/app/includes/footer.php
@@ -7,54 +7,25 @@
 				<?php if ('0' == $Config->read('hide_copyright')) { ?>
 					&copy; <?php echo date('Y'); ?>&nbsp;<?php echo __('BY'); ?> reloxx13 -
 				<?php } ?>
-				<?php
-				$addonVersion = '';
-				$configYaml = dirname(_APPROOT_).'/config.yaml';
-				if (is_readable($configYaml)) {
-				    $yamlContents = file_get_contents($configYaml);
-				    if (false !== $yamlContents && preg_match('/^version:\s*["\']?([^"\'\s]+)["\']?\s*$/m', $yamlContents, $matches)) {
-				        $addonVersion = trim($matches[1]);
-				    }
-				}
-				?>
-				<?php if ('' !== $addonVersion) { ?>
-					OpenBKAdmin v<?php echo htmlspecialchars($addonVersion, ENT_QUOTES, 'UTF-8'); ?> -
-				<?php } ?>
 				<a href='https://github.com/cyberrep/hassio-openbkadmin' target='_blank' rel='noopener noreferrer'>OpenBKAdmin GitHub</a>
 				-
 				<a href='https://github.com/openshwprojects/OpenBK7231T_App' target='_blank' rel='noopener noreferrer'>OpenBeken GitHub</a>
+				- Version 0.6.2
 			</span>
 		</div>
 	</div>
 </footer>
-<div class="modal fade touch-confirm-modal"
-     id="touchToggleConfirmModal"
-     tabindex="-1"
-     aria-labelledby="touchToggleConfirmModalLabel"
-     aria-hidden="true">
+<div class="modal fade touch-confirm-modal" id="touchToggleConfirmModal" tabindex="-1" aria-labelledby="touchToggleConfirmModalLabel" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">
 		<div class="modal-content">
 			<div class="modal-header border-0 pb-0">
-				<h5 class="modal-title toggle-confirm-modal-title" id="touchToggleConfirmModalLabel">
-					<?php echo __('CONFIRM_SWITCHES', 'DEVICES'); ?>
-				</h5>
-				<button type="button"
-				        class="btn-close"
-				        data-bs-dismiss="modal"
-				        aria-label="<?php echo __('CLOSE'); ?>">
-				</button>
+				<h5 class="modal-title toggle-confirm-modal-title" id="touchToggleConfirmModalLabel"><?php echo __('CONFIRM_SWITCHES', 'DEVICES'); ?></h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?php echo __('CLOSE'); ?>"></button>
 			</div>
 			<div class="modal-body toggle-confirm-modal-body pt-2"></div>
 			<div class="modal-footer border-0">
-				<button type="button"
-				        class="btn btn-outline-secondary btn-lg toggle-confirm-modal-cancel"
-				        data-bs-dismiss="modal">
-					<?php echo __('CANCEL'); ?>
-				</button>
-				<button type="button"
-				        class="btn btn-primary btn-lg toggle-confirm-modal-confirm">
-					<?php echo __('CONFIRM'); ?>
-				</button>
+				<button type="button" class="btn btn-outline-secondary btn-lg toggle-confirm-modal-cancel" data-bs-dismiss="modal"><?php echo __('CANCEL'); ?></button>
+				<button type="button" class="btn btn-primary btn-lg toggle-confirm-modal-confirm"><?php echo __('CONFIRM'); ?></button>
 			</div>
 		</div>
 	</div>
